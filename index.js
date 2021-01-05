b1468e47c01aab92cd6cbaf6607e102924cd3a3c
daein <dnfwlxo11@naver.com>
2021-01-05 10:45:31 +0900

    add: simple register router code

--- index.js
+++ index.js
@@ -2,12 +2,18 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const { User } = require('./models/User')
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
 const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://root:root@node-react-boilerplate.agbac.mongodb.net/mgDB?retryWrites=true&w=majority', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
 }).then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err))
 
@@ -15,6 +21,18 @@ mongoose.connect('mongodb+srv://root:root@node-react-boilerplate.agbac.mongodb.n
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.post('/register', (req, res) => {
+  // 회원 가입시 필요한 정보들을 클라이언트에서 가져오면, DB에 넣어줌.
+  const user = new User(req.body)
+
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err})
+    return res.status(200).json({ 
+      success: true
+    })
+  })
 })
 
 app.listen(port, () => {
